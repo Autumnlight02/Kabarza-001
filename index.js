d26e9e0c5c6b12a329bae9387d3b83c5d7d15b92
lukasmargerie <aykut.akguen@outlook.de>
2022-03-18 17:51:50 +0100

    branch 2

--- index.js
+++ index.js
@@ -1,87 +1,126 @@
-const textElements = ["test1", "test2", "kabarza is gay"];
-const textElementsOnDom = [];
+// developed by Aykut Akgün | requested by Kabarza
+// https://twitter.com/AutumnLight20
 
-const containerElement = document.getElementById("container");
+try {
+  // this is the element where the elements will be viewn later on
+  const viewContainer = document.getElementById("viewContainer");
+  // this is the storage element inside of webflow
+  const storageElement = document.getElementById("storageElement");
+  // This is an Element which gets shown / hidden once all elements are in viewContainer
+  const endMenuElement = document.getElementById("endMenu");
+  // This is the reset Button which deletes all element on press
+  const resetElement = document.getElementById("reset");
+  // the name of the image downloard
+  const imageElement = document.getElementById("image");
+  const imageDownloardName = "mainImage";
 
-let currentIndex = 0;
-let currentElement = undefined;
+  //--- do not touch anything under here !!! ---
 
-const data = {
-  mouseOffsetX: 0,
-  mouseOffsetY: 0,
-  mouseX: 0,
-  mouseY: 0,
-  relMouseX: 0,
-  relMouseY: 0,
-};
+  // this is a variable to hold the current element
+  let currentElement = undefined;
 
-function init() {
-  const boundings = containerElement.getBoundingClientRect();
-  data.mouseOffsetX = boundings.x + window.scrollX;
-  data.mouseOffsetY = boundings.y + window.scrollY;
-  currentElement = createTextElement(textElements[0]);
-  document.getElementById("endMenu").style.display = "none";
-}
-init();
+  // These are the current mouse offsets and values
+  const data = {
+    mouseOffsetX: 0,
+    mouseOffsetY: 0,
+    mouseX: 0,
+    mouseY: 0,
+    relMouseX: 0,
+    relMouseY: 0,
+  };
 
-document.addEventListener("scroll", calculateRelativeMousepos);
+  // function which launches on start, it sets the offset of the view element to the mouse and selects the first element
+  function init() {
+    // checking if everything is setup correctly
+    if (viewContainer === null) {
+      alert(
+        "warning, the viewContainer is not found, either you run the code not at the end of the body or you wrote the id wrong"
+      );
+      throw "error";
+    } else if (storageElement === null) {
+      alert("warning, the storageElement which contains the append Elements is not found");
+      throw "error";
+    } else if (storageElement.children.length === 0) {
+      alert(
+        "warning, the storage Element which contains the append Elements is empty, you need atleast one Element in it"
+      );
+      throw "error";
+    } else if (resetElement === null) {
+      alert("warning, there is no Element with the Id: 'resetElement' to reset the board");
+      throw "error";
+    } else if (endMenuElement === null) {
+      alert("warning, there is no Element with the Id: 'endMenuElement' to toggle a menu on all elements shown");
+      throw "error";
+    } else if (imageElement === null) {
+      alert("warning, there is no Element with the Id: 'imageElement' to downloard images");
+      throw "error";
+    }
+    const boundings = viewContainer.getBoundingClientRect();
+    data.mouseOffsetX = boundings.x + window.scrollX;
+    data.mouseOffsetY = boundings.y + window.scrollY;
+    currentElement = storageElement.children[0];
 
-containerElement.addEventListener("click", nextText);
-function nextText() {
-  currentIndex++;
-  if (currentIndex >= textElements.length) {
-    currentElement = undefined;
-    endElement.style.display = "block";
-  } else {
-    currentElement = createTextElement(textElements[currentIndex]);
+    for (let i = 0; i < storageElement.children.length; i++) {
+      storageElement.children[i].classList.add("dropElement");
+    }
+
+    viewContainer.appendChild(currentElement);
+    document.getElementById("endMenu").style.display = "none";
   }
-}
+  init();
 
-containerElement.addEventListener("mousemove", moveText);
-function moveText(event) {
-  data.mouseX = event.clientX;
-  data.mouseY = event.clientY;
-  calculateRelativeMousepos();
-}
+  document.addEventListener("scroll", calculateRelativeMousepos);
 
-function calculateRelativeMousepos() {
-  data.relMouseX = window.scrollX + data.mouseX - data.mouseOffsetX;
-  data.relMouseY = window.scrollY + data.mouseY - data.mouseOffsetY;
-  if (currentElement !== undefined) {
-    currentElement.style.top = data.relMouseY + "px";
-    currentElement.style.left = data.relMouseX + "px";
+  viewContainer.addEventListener("click", nextText);
+  function nextText() {
+    if (storageElement.children.length === 0) {
+      currentElement = undefined;
+      endMenuElement.style.display = "block";
+    } else {
+      currentElement = storageElement.children[0];
+      viewContainer.appendChild(currentElement);
+    }
   }
-  // console.log(data.relMouseX, data.relMouseY);
-}
 
-function createTextElement(text) {
-  const textContainer = document.createElement("div");
-  textContainer.classList.add("dropElement");
-  textContainer.textContent = text;
-  containerElement.appendChild(textContainer);
-  textElementsOnDom.push(textContainer);
-  return textContainer;
-}
-
-document.getElementById("image").addEventListener("click", downloardImage);
-function downloardImage() {
-  const element = containerElement;
-  const imageName = "test";
-  domtoimage.toPng(element).then(function (dataUrl) {
-    var link = document.createElement("a");
-    link.download = imageName + ".png";
-    link.href = dataUrl;
-    link.click();
-  });
-}
-document.getElementById("reset").addEventListener("click", reset);
-function reset() {
-  for (let i = 0; i < textElementsOnDom.length; i++) {
-    textElementsOnDom[i].remove();
+  // mouse event
+  viewContainer.addEventListener("mousemove", moveText);
+  function moveText(event) {
+    data.mouseX = event.clientX;
+    data.mouseY = event.clientY;
+    calculateRelativeMousepos();
   }
-  currentElement = createTextElement(textElements[0]);
-  currentIndex = 0;
-  endElement.style.display = "none";
-}
+  function calculateRelativeMousepos() {
+    data.relMouseX = window.scrollX + data.mouseX - data.mouseOffsetX;
+    data.relMouseY = window.scrollY + data.mouseY - data.mouseOffsetY;
+    if (currentElement !== undefined) {
+      currentElement.style.top = data.relMouseY + "px";
+      currentElement.style.left = data.relMouseX + "px";
+    }
+  }
 
-const endElement = document.getElementById("endMenu");
+  // function to take an image of the viewContainer
+  imageElement.addEventListener("click", downloardImage);
+  function downloardImage() {
+    const element = viewContainer;
+    domtoimage.toPng(element).then(function (dataUrl) {
+      var link = document.createElement("a");
+      link.download = imageDownloardName + ".png";
+      link.href = dataUrl;
+      link.click();
+    });
+  }
+
+  // function to reset all elements in the viewContainer
+  resetElement.addEventListener("click", reset);
+  function reset() {
+    let length = viewContainer.children.length;
+    for (let i = 0; i < length; i++) {
+      storageElement.appendChild(viewContainer.children[0]);
+    }
+    currentElement = storageElement.children[0];
+    viewContainer.appendChild(currentElement);
+    endMenuElement.style.display = "none";
+  }
+} catch (err) {
+  console.log(err);
+}
